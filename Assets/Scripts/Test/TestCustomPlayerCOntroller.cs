6b05b3a2d0865d46c8d867b130e1ed70785624d9
Roger Lozano <lozanofierroruu@gmail.com>
2026-02-28 00:02:28 +0100

    feat: Smooth rotation for the camera

--- Assets/Scripts/Test/TestCustomPlayerCOntroller.cs
+++ Assets/Scripts/Test/TestCustomPlayerCOntroller.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Assets.Scripts.Test
@@ -9,6 +8,10 @@ namespace Assets.Scripts.Test
         // ================================================== MEMBERS ==================================================
         [SerializeField]
         private float m_speed = 5f;
+
+        [SerializeField]
+        private float m_rotationSpeed = 1f;
+
         private InputSystem_Actions m_inputSystemAction;
         private CharacterController m_characterController;
         private Vector2 m_inputMove = new Vector2(0f, 0f);
@@ -44,7 +47,25 @@ namespace Assets.Scripts.Test
 
         private void Move()
         {
-            Vector3 direction = new Vector3(m_inputMove.x, 0f, m_inputMove.y);
+            Vector3 cameraForward = UnityEngine.Camera.main.transform.forward;
+            cameraForward.y = 0f;
+            cameraForward.Normalize();
+
+            Vector3 cameraRight = UnityEngine.Camera.main.transform.right;
+            cameraRight.y = 0f;
+            cameraRight.Normalize();
+
+            Vector3 direction = (cameraForward * m_inputMove.y) + (cameraRight * m_inputMove.x);
+            if (direction != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(
+                    transform.rotation,
+                    targetRotation,
+                    m_rotationSpeed * Time.deltaTime
+                );
+            }
+
             m_characterController.Move(m_speed * Time.deltaTime * direction);
         }
     }
