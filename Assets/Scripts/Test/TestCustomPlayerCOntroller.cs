ab124996ada4be0f5dd90d84f03b2c3f385d1374
Roger Lozano <lozanofierroruu@gmail.com>
2026-02-27 07:46:24 +0100

    feat: Add the horizontal movement. i active the input system actintos settings, cerated the necessary memberts for the movement and added 2 methods: for read the keyvalue and for the move

--- Assets/Scripts/Test/TestCustomPlayerCOntroller.cs
+++ Assets/Scripts/Test/TestCustomPlayerCOntroller.cs
@@ -3,13 +3,46 @@ using UnityEngine;
 
 namespace Assets.Scripts.Test
 {
+    [RequireComponent(typeof(CharacterController))]
     public class TestCustomPlayerCOntroller : MonoBehaviour
     {
-        private InputSystem_Actions _inputSystemAction;
+        [SerializeField]
+        private float m_speed = 5f;
+        private InputSystem_Actions m_inputSystemAction;
+        private CharacterController m_characterController;
+        private Vector2 m_inputMove = new Vector2(0f, 0f);
 
         private void Awake()
         {
-            _inputSystemAction = new InputSystem_Actions();
+            m_inputSystemAction = new InputSystem_Actions();
+            m_characterController = GetComponent<CharacterController>();
+        }
+
+        private void OnEnable()
+        {
+            m_inputSystemAction.Player.Move.Enable();
+        }
+
+        private void OnDisable()
+        {
+            m_inputSystemAction.Player.Move.Disable();
+        }
+
+        private void Update()
+        {
+            ReadInput();
+            Move();           
+        }
+
+        private void ReadInput()
+        {
+            m_inputMove = m_inputSystemAction.Player.Move.ReadValue<Vector2>();
+        }
+
+        private void Move()
+        {
+            var direction = new Vector3(m_inputMove.x, 0f, m_inputMove.y);
+            m_characterController.Move(m_speed * Time.deltaTime * direction);
         }
     }
 }
