92752306724399ff86e69a40c9ebb45d20a386c3
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-12 00:18:05 +0100

    feat: Read the shoot only in the pressed frame. Insatntiate de prefab bullet, get the rotation and rigidbody and throw with serialized force

--- Assets/Scripts/Gun/Shoot.cs
+++ Assets/Scripts/Gun/Shoot.cs
@@ -4,6 +4,56 @@ namespace Assets.Scripts.Gun
 {
     public class Gun : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject _bullet;
+        [SerializeField] private Transform _firePoint;
+        [SerializeField] private float _shootForce = 5f;
+        private InputSystem_Actions _inputSystemActions;
+
+        private void Awake()
+        {
+            _inputSystemActions = new InputSystem_Actions();
+        }
+
+        private void OnEnable()
+        {
+            _inputSystemActions.Player.Shoot.Enable(); 
+        }
+
+        private void OnDisable()
+        {
+            _inputSystemActions.Player.Shoot.Disable(); 
+        }
+
+        private void Update()
+        {
+            ToShoot();
+        }
+
+        private void ToShoot()
+        {
+            if (_inputSystemActions.Player.Shoot.WasPressedThisFrame())
+            {
+                Vector3 shootDirection = GetShootDirection();
+                Quaternion shootRotation = Quaternion.LookRotation(shootDirection);
+                GameObject bulletInstance = Instantiate(_bullet, _firePoint.position, shootRotation);
+                var rigidbodyBullet = bulletInstance.GetComponent<Rigidbody>();
+
+                if (rigidbodyBullet != null)
+                {
+                    rigidbodyBullet.linearVelocity = shootDirection * _shootForce;
+                }
+            }   
+        }
+
+        private Vector3 GetShootDirection()
+        {
+            if (UnityEngine.Camera.main != null)
+            {
+                return UnityEngine.Camera.main.transform.forward.normalized;
+            }
+
+            return _firePoint.forward.normalized;
+        }
+
     }
 }
