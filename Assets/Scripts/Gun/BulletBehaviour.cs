f170660a07ab447b47a227b8f5648ed252d3c5a2
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-18 20:06:03 +0100

    feat: Destroy the object in 7 seconds or detected a collision

--- Assets/Scripts/Gun/BulletBehaviour.cs
+++ Assets/Scripts/Gun/BulletBehaviour.cs
@@ -4,6 +4,14 @@ namespace Assets.Scripts.Gun
 {
     public class BulletBehaviour : MonoBehaviour
     {
-        
+        private void Start()
+        {
+            Destroy(this.gameObject, 7f);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
