9982bdd4ce65dfe2870235557b41d155071e8fd3
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-23 22:46:30 +0100

    wip: Start script to handle pun

---
+++ Assets/Scripts/Network/NetworkConroller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+namespace Assets.Scripts.Network
+{
+    public class NetworkController : MonoBehaviourPunCallbacks
+    {
+        
+    }
+}
