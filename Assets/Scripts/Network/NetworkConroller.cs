eb58cec48e95c72b04a609d4b1be60ce181fc1b2
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-23 23:05:19 +0100

    feat: Add connection to PUN and create room fro 2 playwers

--- Assets/Scripts/Network/NetworkConroller.cs
+++ Assets/Scripts/Network/NetworkConroller.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
-using Photon;
 using Photon.Pun;
+using Photon.Realtime;
 
 namespace Assets.Scripts.Network
 {
     public class NetworkController : MonoBehaviourPunCallbacks
     {
-        
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            PhotonNetwork.JoinOrCreateRoom(
+                "Adventure",
+                new RoomOptions {MaxPlayers = 2},
+                null
+            );
+        }
+
+        public override void OnJoinedRoom()
+        {
+            // PhotonNetwork.Instantiate();
+        }
     }
 }
