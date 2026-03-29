98a0c08211405c84f908c5a22597c6894a0d37fb
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-30 00:22:23 +0200

    feat: logical with tags player, to gt the ammo or dissactive in scene

--- Assets/Scripts/Managers/AmmoHandler.cs
+++ Assets/Scripts/Managers/AmmoHandler.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using Assets.Scripts.Gun;
 using Assets.Scripts.Ammo;
+using Assets.Scripts.Player;
 
 namespace Assets.Scripts.Managers
 {
@@ -9,18 +10,33 @@ namespace Assets.Scripts.Managers
         /* ================================================================================================================
         ---------------------------------------------------- MEMBERS -----------------------------------------------------
         ================================================================================================================= */
-        [SerializeField] private GameObject _ammo;
-        [SerializeField] private Shoot _shootScript;
-        [SerializeField] private GetAmmo _getAmmoScript;
+        private Shoot _shootScript;
+        private GetAmmo _getAmmoScript;
+        private GameObject _ammoPlayer1;
+        private GameObject _ammoPlayer2;
+        private GameObject _ammoPlayer3;
         private float _randomX;
         private float _randomZ;
 
         /* ================================================================================================================
         ---------------------------------------------------- UNITY LIFE CYCLE METHODS -----------------------------------------------------
         ================================================================================================================= */
-
+        private void Awake()
+        {
+            _ammoPlayer1 = FindInactiveObjectWithTag("Ammo1");
+            _ammoPlayer2 = FindInactiveObjectWithTag("Ammo2");
+            _ammoPlayer3 = FindInactiveObjectWithTag("Ammo3");
+        }
         private void Start()
         {
+            _ammoPlayer1.SetActive(false);
+            _ammoPlayer2.SetActive(false);
+            _ammoPlayer3.SetActive(false);
+
+            _shootScript = transform.root.GetComponentInChildren<Shoot>();
+            _getAmmoScript = transform.root.GetComponent<GetAmmo>();
+            // Invoke(nameof(SuscribeEvents), 5f);
+
             _shootScript.OnShowAmmo += ActiveAmmo;
             _getAmmoScript.OnDisableAmmo += DisableAmmo;
         }
@@ -31,22 +47,69 @@ namespace Assets.Scripts.Managers
             _getAmmoScript.OnDisableAmmo -= DisableAmmo;
         }
 
+        // private void SuscribeEvents()
+        // {
+        //     _shootScript.OnShowAmmo += ActiveAmmo;
+        //     _getAmmoScript.OnDisableAmmo += DisableAmmo;
+        // }
+
+        /* ================================================================================================================
+        ---------------------------------------------------- ASSING AMMO -----------------------------------------------------
+        ================================================================================================================= */
+        
+        private GameObject FindInactiveObjectWithTag(string tag)
+        {
+            GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+            foreach (GameObject obj in allObjects)
+            {
+                if (obj.CompareTag(tag) && obj.hideFlags == HideFlags.None)
+                {
+                    return obj;
+                }
+            }
+            return null;
+        }
+
         /* ================================================================================================================
         ---------------------------------------------------- ENABLE/DISABLE AMMO -----------------------------------------------------
         ================================================================================================================= */
 
-        private void ActiveAmmo()
+        private void ActiveAmmo(string playerTag)
         {
-            _randomX = Random.Range(-24f, 24f);
-            _randomZ = Random.Range(-24f, 24f);
-            _ammo.transform.position = new Vector3(_randomX, 1f, _randomZ);
-
-            _ammo.gameObject.SetActive(true);
+            if (playerTag == "Player")
+            {
+                _randomX = Random.Range(-24f, 24f);
+                _randomZ = Random.Range(-24f, 24f);
+                _ammoPlayer1.transform.position = new Vector3(_randomX, 1f, _randomZ);
+                _ammoPlayer1.gameObject.SetActive(true);
+            }
+            if (playerTag == "Player2")
+            {
+                _randomX = Random.Range(-24f, 24f);
+                _randomZ = Random.Range(-24f, 24f);
+                _ammoPlayer2.transform.position = new Vector3(_randomX, 1f, _randomZ);
+                _ammoPlayer2.gameObject.SetActive(true);
+            }
+            if (playerTag == "Player3")
+            {
+                _randomX = Random.Range(-24f, 24f);
+                _randomZ = Random.Range(-24f, 24f);
+                _ammoPlayer3.transform.position = new Vector3(_randomX, 1f, _randomZ);
+                _ammoPlayer3.gameObject.SetActive(true);
+            }
         }
 
-        private void DisableAmmo()
+        private void DisableAmmo(string playerTag)
         {
-            _ammo.gameObject.SetActive(false);
+            if (playerTag == "Player")
+                _ammoPlayer1.gameObject.SetActive(false);
+
+            if (playerTag == "Player2")
+                _ammoPlayer2.gameObject.SetActive(false);
+
+            if (playerTag == "Player3")
+                _ammoPlayer3.gameObject.SetActive(false);
         }
+
     }
 }
