64277bbb9d25f32e297d0de3ee7c33437e060e46
Roger Lozano <lozanofierroruu@gmail.com>
2026-03-30 19:18:34 +0200

    feat: Conditional to diseable player's componnets if is not local

--- Assets/Scripts/Player/PlayerSetup.cs
+++ Assets/Scripts/Player/PlayerSetup.cs
@@ -6,7 +6,24 @@ namespace Assets.Scripts.Player
 {
     public class PlayerSetup : MonoBehaviour
     {
+        private PhotonView _photonViewScript;
+        private UnityEngine.Camera _cameraPlayer;
+        private AudioListener _audioListenerPlayer;
 
+        private void Start()
+        {
+            // Fields
+            _photonViewScript = GetComponent<PhotonView>();
+            _cameraPlayer = GetComponentInChildren<UnityEngine.Camera>();
+            _audioListenerPlayer = GetComponentInChildren<AudioListener>();
+
+            // Logical settings
+            if (!_photonViewScript.IsMine)
+            {
+                _cameraPlayer.enabled = false;
+                _audioListenerPlayer.enabled = false;
+            }       
+        }
 
     }
 }
